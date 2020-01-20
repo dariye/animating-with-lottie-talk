bbdf417b4ee3fefb38d7b1feb485c04ebd8e181e
paul <paul.dariye@gmail.com>
2020-01-20 07:26:03 +0100

    Initial commit

---
+++ demos/vanilla-js/gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+gulp.task('sw', function (callback) {
+    const swPrecache = require('sw-precache');
+    const appDir = 'app';
+    swPrecache.write(`${appDir}/sw.js`, {
+        staticFileGlobs: [
+            appDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}',
+        ],
+        stripPrefix: appDir
+    }, callback)
+})
+
+gulp.task('default', ['sw'])
---
+++ demos/vanilla-js/src/js/index.js
@@ -0,0 +1,31 @@
+import lottie from 'lottie-web/build/player/lottie_light'
+import animationData from '../animation/data.json'
+let lottieInstance = null
+
+function initLottie (element) {
+    const style = {
+        window: '100%',
+        height: '100%',
+        overflow: 'hidden',
+        outline: 'none'
+    }
+    const defaultConfig = {
+        loop: true,
+        autoplay: true,
+        renderer: 'svg',
+    }
+    const options = {container: element,
+    animationData,...defaultConfig}
+
+    if (!lottieInstance && element) {
+        element.style = style
+        lottieInstance = lottie.loadAnimation(options)
+    }
+}
+
+function main () {
+    const targetElement = document.getElementById('lottie')
+    initLottie(targetElement)
+}
+
+window.onload = main
